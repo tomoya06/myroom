611d4ac64cd0fb7dbb947fcc09605043a1a165da
tomoya06 <peng4766@gmail.com>
2022-10-02 20:56:09 +0800

    feat: iframe page

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "pages/ComputerScreen/index.html"),
+      },
+    },
+  },
+});
