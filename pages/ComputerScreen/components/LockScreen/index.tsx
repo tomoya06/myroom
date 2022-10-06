b54d46e7a5e1f47978509160100b916f56915d93
tomoya06 <peng4766@gmail.com>
2022-10-07 01:30:17 +0800

    feat: system loading process

--- pages/ComputerScreen/components/LockScreen/index.tsx
+++ pages/ComputerScreen/components/LockScreen/index.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import DotsLoading from "../../partials/DotsLoading";
+import TextLoop from "../../partials/TextLoop";
 import "./index.scss";
 
 interface Props {
@@ -15,6 +16,21 @@ const LockScreen: React.FC<Props> = (props) => {
       {loading && (
         <>
           <DotsLoading />
+          <div style={{ margin: `1rem 0 0 0` }}></div>
+          {!unlocked && (
+            <TextLoop
+              int={5000}
+              texts={[
+                "Installing system apps...",
+                "Clearing cache...",
+                "Optimizing system apps...",
+                "Optimizing system...",
+                "Running graphic test...",
+                "Updating graphic settings...",
+                "Optimizing graphic quality...",
+              ]}
+            />
+          )}
         </>
       )}
     </div>
