8f3abe38fb8e8a42f6edbd7cbf183d87ae2e8dc1
ITIStomoya06 <35499042+tomoya06@users.noreply.github.com>
2022-10-08 15:21:36 +0800

    feat: deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/myroom/",
-  server: {
-    host: true,
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-      "@pages": resolve(__dirname, "./pages"),
+export default ({ mode }) => {
+  const base = mode === "github" ? "/myroom/" : "";
+
+  return defineConfig({
+    plugins: [react()],
+    base,
+    server: {
+      host: true,
     },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "pages/ComputerScreen/index.html"),
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+        "@pages": resolve(__dirname, "./pages"),
       },
-      plugins: [visualizer()],
     },
-  },
-});
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          nested: resolve(__dirname, "pages/ComputerScreen/index.html"),
+        },
+        plugins: [visualizer()],
+      },
+    },
+  });
+};
